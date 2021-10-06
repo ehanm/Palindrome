2aefb023b076328856346981cf9ae50ef8a313b7
U-BSD\452735 <452735@SHS-10DCE0A79.beaverton.k12.or.us>
2021-10-06 11:38:51 -0700

    final

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,39 +1,52 @@
+// Made by Ehan Masud
+// Completed on 9/28/21
+// This program takes an input (with a maximum of 80 characters long) and determines whether it is a palindrome or not
 #include <iostream>
 #include <cstring>
 
 using namespace std;
 
 int main() {
-  char input[80];
-  char converter[80];
-  char checker[80];
+  char input[81]; // character array for input
+  char converter[81]; // character array to remove all spaces and punctuation 
+  char checker[81]; // character array to reverse the the previous array
   int convertercounter = 0;
   int finalcheck = 0;
-  cin.get(input, 80);
+  memset(input, 0, 81);
+  memset(converter, 0, 81);
+  memset(checker, 0, 81);
 
-  for (int i = 0; i < strlen(input); i++) {
+  cin.get(input, 81);
+  
+  for (int i = 0; i < strlen(input); i++) { // removing all punctuation and converting the entire array to uppercase
     if (isalpha(input[i]) != 0){
       converter[convertercounter] = toupper(input[i]);
       convertercounter++;
+      cout << converter[i] << endl;
     }
  
   }
 
-  for (int i = 0; i < strlen(converter); i++){
+  checker[strlen(converter)] = '\0';
+  
+  for (int i = 0; i < strlen(converter); i++){ // reversing the entire array and copying it into a new array
     checker[i] = converter[strlen(converter)-1-i];
+    cout << checker[i] << endl;
   }
 
-  for (int i = 0; i < strlen(converter); i++) {
+  
+
+  for (int i = 0; i < strlen(converter); i++) { // checking if both arrays are the same
     if (checker[i] == converter[i]){
       finalcheck++;
     }
   }
 
-  if (finalcheck == strlen(converter)) {
+  if (finalcheck == strlen(converter)) { // if they are, print "Palindrome"
     cout << "Palindrome.";
   }
-  else {
-    cout << "Not a palindrome.";
+  else { // if they aren't print "Not a palindrome."
+    cout << "Not a palindrome."; 
   }
  
   return 0;
